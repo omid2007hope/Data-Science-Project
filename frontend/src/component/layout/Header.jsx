363c7fcf35d8003a22a1a66155f7f1e67f0ce66d
omid2007hope <omid85hope@gmail.com>
2026-01-17 01:15:05 +0100

    Better CSS

--- frontend/src/component/layout/Header.jsx
+++ frontend/src/component/layout/Header.jsx
@@ -2,31 +2,37 @@ import {
   CSS_flexRow,
   CSS_fixPosition,
   CSS_color_darkest,
+  CSS_color_dark,
+  CSS_color_light,
+  CSS_color_lightest,
   CSS_text_hover_active_colorBase,
+  CSS_responsiveTextSize,
 } from "../../libraries/CSS_General";
 
 function Header() {
   const HeaderMenuOption = [
     { id: 1, option: "Home" },
-    { id: 2, option: "option-1" },
-    { id: 3, option: "option-2" },
-    { id: 4, option: "option-3" },
+    { id: 2, option: "account" },
+    { id: 3, option: "about" },
+    { id: 4, option: "terms of service" },
   ];
 
   return (
     <>
       <header
-        className={`justify-center w-full h-1/20 border-b ${CSS_flexRow} ${CSS_color_darkest} ${CSS_fixPosition}`}
+        className={`justify-center w-full h-7/100 md:h-5/100 lg:h-5/100 border-b ${CSS_flexRow} ${CSS_color_darkest} ${CSS_fixPosition}`}
       >
-        <div className={`justify-evenly w-1/3 h-full ${CSS_flexRow}`}>
+        <div
+          className={`justify-evenly w-2/3 md:w-1/3 lg:w-1/3 h-full ${CSS_flexRow}`}
+        >
           {HeaderMenuOption.map((item) => {
             return (
               <div
                 key={item.id}
-                className={`justify-center w-1/4 h-full ${CSS_flexRow}`}
+                className={`justify-center w-1/4 h-full ${CSS_flexRow} ${CSS_color_darkest}`}
               >
                 <button
-                  className={`justify-around ${CSS_text_hover_active_colorBase} ${CSS_flexRow} ${CSS_color_darkest}`}
+                  className={`justify-around ${CSS_responsiveTextSize} ${CSS_text_hover_active_colorBase} ${CSS_flexRow} ${CSS_color_darkest}`}
                 >
                   {item.option}
                 </button>
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,38 +1,63 @@
+import { useState } from "react";
 import {
   CSS_flexCol,
   CSS_color_darkest,
+  CSS_color_dark,
+  CSS_color_light,
+  CSS_color_lightest,
   CSS_H_W_full,
   CSS_border_left,
   CSS_border_right,
   CSS_flexRow,
   CSS_generalBorder,
+  CSS_text_hover_active_colorBase,
+  CSS_responsive_input_btn_height,
+  CSS_responsive_btn_width,
 } from "../libraries/CSS_General";
 
 import { Search } from "lucide-react";
 
 function Home() {
+  const [search, setSearch] = useState("");
+
+  const searchSomething = () => {
+    if (search) {
+      console.log(search);
+      setSearch();
+    } else {
+      alert("Input is empty");
+      return;
+    }
+  };
+
   return (
     <>
       <div
-        className={`justify-center pt-14 ${CSS_H_W_full} ${CSS_flexCol} ${CSS_color_darkest}`}
+        className={`justify-center pt-14 ${CSS_H_W_full} ${CSS_flexCol} ${CSS_color_lightest}`}
       >
         <div
-          className={`w-4/10 md:w-8/10 h-9/10 justify-between ${CSS_generalBorder} ${CSS_flexCol} ${CSS_color_darkest}`}
+          className={`w-8/10 md:w-8/10 h-9/10 justify-between ${CSS_generalBorder} ${CSS_flexCol}`}
         >
           <div
-            className={`justify-center w-full h-1/10 border-b ${CSS_flexRow} ${CSS_color_darkest} `}
+            className={`justify-center w-full h-1/10 border-b ${CSS_flexRow} ${CSS_color_darkest}`}
           >
             <input
               type="text"
-              className={`pl-2.5 pr-120 py-2.5 ${CSS_color_darkest} ${CSS_border_left}`}
+              className={`pl-2.5  w-60 md:w-100 lg:w-100 ${CSS_responsive_input_btn_height} ${CSS_color_dark} ${CSS_border_left}`}
+              onChange={(e) => setSearch(e.target.value.trim())}
             />
             <button
-              className={`py-2.5 px-3 ${CSS_color_darkest} ${CSS_border_right} ${CSS_flexCol}`}
+              className={`${CSS_responsive_btn_width} ${CSS_responsive_input_btn_height} ${CSS_color_darkest} ${CSS_border_right} ${CSS_flexCol} ${CSS_text_hover_active_colorBase}`}
+              onClick={() => searchSomething()}
             >
               <Search />
             </button>
           </div>
-          <div className={`justify-center w-full h-9/10 ${CSS_flexCol}`}></div>
+          <div
+            className={`justify-center w-full h-9/10 ${CSS_flexCol} ${CSS_color_light}`}
+          >
+            s
+          </div>
         </div>
       </div>
     </>
