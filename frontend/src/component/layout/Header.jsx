7a679ad9f709d90104a86083ac81823358666773
omid2007hope <omid85hope@gmail.com>
2026-01-18 02:12:58 +0100

    CSS perfection

--- frontend/src/component/layout/Header.jsx
+++ frontend/src/component/layout/Header.jsx
@@ -1,12 +1,11 @@
 import {
   CSS_FIXED_Z50,
-  CSS_H_FULL,
   CSS_RESPONSIVE_TEXT_SIZE,
-  CSS_SURFACE_DARKER,
   CSS_TEXT_HOVER_ACTIVE_COLOR_BASE,
   CSS_flexRow_around,
   CSS_flexRow_center,
   CSS_flexRow_evenly,
+  CSS_COLOR_950,
 } from "../../libraries/CSS_Main";
 
 function Header() {
@@ -20,19 +19,19 @@ function Header() {
   return (
     <>
       <header
-        className={`w-full h-7/100 md:h-5/100 lg:h-5/100 border-b border-slate-700 ${CSS_flexRow_center} ${CSS_SURFACE_DARKER} ${CSS_FIXED_Z50}`}
+        className={`w-full h-7/100 md:h-5/100 lg:h-5/100 border-b border-slate-700 ${CSS_flexRow_center} ${CSS_COLOR_950} ${CSS_FIXED_Z50}`}
       >
         <div
-          className={`w-full md:w-1/3 lg:w-1/3 ${CSS_H_FULL} ${CSS_flexRow_evenly}`}
+          className={`w-full md:w-1/3 lg:w-1/3 h-full ${CSS_flexRow_evenly}`}
         >
           {HeaderMenuOption.map((item) => {
             return (
               <div
                 key={item.id}
-                className={`w-1/4 ${CSS_H_FULL} ${CSS_flexRow_center} ${CSS_SURFACE_DARKER}`}
+                className={`w-1/4 h-full ${CSS_flexRow_center} ${CSS_COLOR_950}`}
               >
                 <button
-                  className={`${CSS_RESPONSIVE_TEXT_SIZE} ${CSS_TEXT_HOVER_ACTIVE_COLOR_BASE} ${CSS_flexRow_around} ${CSS_SURFACE_DARKER}`}
+                  className={`${CSS_RESPONSIVE_TEXT_SIZE} ${CSS_TEXT_HOVER_ACTIVE_COLOR_BASE} ${CSS_flexRow_around} ${CSS_COLOR_950}`}
                 >
                   {item.option}
                 </button>
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -2,19 +2,20 @@ import API from "../api/index";
 
 import { useState } from "react";
 import {
-  CSS_BORDER,
   CSS_BORDER_Y_LEFT,
   CSS_BORDER_Y_RIGHT,
-  CSS_H_FULL,
+  CSS_RING,
+  CSS_SHADOW_SM,
   CSS_RESPONSIVE_BTN_SIZE,
   CSS_RESPONSIVE_INPUT_HEIGHT,
-  CSS_SURFACE_DARK,
-  CSS_SURFACE_DARKER,
-  CSS_SURFACE_LIGHT,
   CSS_TEXT_HOVER_ACTIVE_COLOR_BASE,
   CSS_WH_full,
+  CSS_flexCol_between,
   CSS_flexCol_center,
   CSS_flexRow_center,
+  CSS_COLOR_50,
+  CSS_COLOR_950,
+  CSS_COLOR_700,
 } from "../libraries/CSS_Main";
 
 import { Search } from "lucide-react";
@@ -36,32 +37,32 @@ function Home() {
   return (
     <>
       <div
-        className={`justify-center pt-14 pb-4 ${CSS_WH_full} ${CSS_flexCol_center} ${CSS_SURFACE_LIGHT}`}
+        className={`justify-center pt-14 pb-4 ${CSS_WH_full} ${CSS_flexCol_center} ${CSS_COLOR_50}`}
       >
         <div
-          className={`w-9/10 md:w-8/10 h-10/10 md:h-9/10 lg:h-9/10 justify-between ${CSS_BORDER} ${CSS_flexCol_center}`}
+          className={`w-9/10 md:w-8/10 h-10/10 md:h-9/10 lg:h-9/10 ${CSS_flexCol_between} ${CSS_RING} ${CSS_SHADOW_SM}`}
         >
           <div
-            className={`justify-center w-full h-1/10 border-b border-slate-700 ${CSS_flexRow_center} ${CSS_SURFACE_DARKER}`}
+            className={`w-full h-1/10 border-b ${CSS_flexRow_center} ${CSS_COLOR_950}`}
           >
             <input
               type="text"
               value={search}
-              className={`pl-2.5 w-60 md:w-100 lg:w-100 ${CSS_RESPONSIVE_INPUT_HEIGHT} ${CSS_SURFACE_DARK} ${CSS_BORDER_Y_LEFT}`}
+              className={`pl-2.5 w-60 md:w-100 lg:w-100 ${CSS_RESPONSIVE_INPUT_HEIGHT} ${CSS_COLOR_700} ${CSS_BORDER_Y_LEFT}`}
               onChange={(e) => setSearch(e.target.value.trim())}
               onKeyDown={(e) => {
                 if (e.key === "Enter") searchSomething();
               }}
             />
             <button
-              className={`${CSS_RESPONSIVE_BTN_SIZE} ${CSS_RESPONSIVE_INPUT_HEIGHT} ${CSS_SURFACE_DARKER} ${CSS_BORDER_Y_RIGHT} ${CSS_flexCol_center} ${CSS_TEXT_HOVER_ACTIVE_COLOR_BASE}`}
+              className={`${CSS_RESPONSIVE_BTN_SIZE} ${CSS_RESPONSIVE_INPUT_HEIGHT} ${CSS_COLOR_950} ${CSS_BORDER_Y_RIGHT} ${CSS_flexCol_center} ${CSS_TEXT_HOVER_ACTIVE_COLOR_BASE}`}
               onClick={() => searchSomething()}
             >
               <Search />
             </button>
           </div>
           <div
-            className={`justify-center w-full h-9/10 ${CSS_flexCol_center} ${CSS_SURFACE_LIGHT}`}
+            className={`justify-center w-full h-9/10 ${CSS_flexCol_center} ${CSS_COLOR_50}`}
           >
             s
           </div>
